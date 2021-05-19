9a318a3a09c99cc17b88e7428207016349b34a05
Matt Kragen <mattdkragen@gmail.com>
2021-05-19 13:56:29 -0700

    Create animals_older_than test

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -55,4 +55,20 @@ RSpec.describe Zoo do
         expect(zoo.animal_count).to eq(2)
     end
 
+    it 'lists animals older than given age' do
+        zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+        animal_1 = Animal.new("Sea Otter", 10, 25)
+        animal_2 = Animal.new("Red Panda", 5, 70)
+        animal_3 = Animal.new("Capybara", 100, 150)
+        animal_4 = Animal.new("Dolphin", 150, 200)
+        zoo.add_animal(animal_1)
+        zoo.add_animal(animal_2)
+        zoo.add_animal(animal_3)
+        zoo.add_animal(animal_4)
+
+        expect(zoo.animals_older_than(250)).to eq([])
+        expect(zoo.animals_older_than(100)).to eq([])
+        expect(zoo.animals_older_than(10)).to eq([])
+    end
+
 end
