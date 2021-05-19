46de07733bd984f2af46444a59a52cdfb2ddd192
Matt Kragen <mattdkragen@gmail.com>
2021-05-19 13:35:16 -0700

    Add Zoo class and existence test

---
+++ lib/zoo.rb
@@ -0,0 +1,9 @@
+class Zoo
+    def initialize(street, city, state, zip_code, address)
+        @street = street
+        @city = city
+        @state = state
+        @zip_code = zip_code
+        @address = address
+    end
+end
