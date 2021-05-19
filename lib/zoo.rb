bc74aed69ae52861b88fa5eae520a5b8e8e58ecc
Matt Kragen <mattdkragen@gmail.com>
2021-05-19 13:46:50 -0700

    Create address test; create address method

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -1,9 +1,19 @@
 class Zoo
-    def initialize(street, city, state, zip_code, address)
+    attr_reader :name,
+                :street,
+                :city,
+                :state,
+                :zip_code
+
+    def initialize(name, street, city, state, zip_code)
+        @name = name
         @street = street
         @city = city
         @state = state
         @zip_code = zip_code
-        @address = address
+    end
+
+    def address
+        "#{@street} #{@city}, #{@state} #{@zip_code}"
     end
 end
