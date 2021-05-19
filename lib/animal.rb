fe6c8ad56b47df59f0719f7179c6fdcb2a65d46b
Matt Kragen <mattdkragen@gmail.com>
2021-05-19 13:21:43 -0700

    Complete Animal class & test for functionality

---
+++ lib/animal.rb
@@ -0,0 +1,21 @@
+class Animal
+    attr_reader :kind
+
+    def initialize(kind, weight, age)
+        @kind = kind
+        @weight = weight
+        @age = age
+    end
+
+    def weight
+        "#{@weight} pounds"
+    end
+
+    def age
+        "#{@age} weeks"
+    end
+
+    def age_in_days
+        @age * 7
+    end
+end
